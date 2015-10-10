2fb4ec1f0efaf5ac095e33cd930fc01bf6d54858
frazchaudhry <fraz501@hotmail.com>
2015-10-10 14:13:31 -0500

    Some change
    some change

---
+++ SportsStore.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private IProductsRepository repository;
+
+        public DefaultController(IProductsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            var products = repository.Products.ToList();
+            return View(products);
+        }
+    }
+}
